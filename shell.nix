824379717ffd9aa4dc63e8a6a9722ad3d57e214b
Second-Last <69079342+Second-Last@users.noreply.github.com>
2024-03-02 17:24:41 -0500

    Update shell.nix to include CS400-Lib as well

--- shell.nix
+++ shell.nix
@@ -12,6 +12,35 @@ let
 
   pkgs = import nixpkgs {};
 
+  agdaPkgs = pkgs.agdaPackages;
+
+  # Our class library
+  cs400-lib = agdaPkgs.mkDerivation {
+    pname = "cs400-lib";
+
+    version = "0.0.1";
+
+    src = pkgs.fetchFromGitHub {
+      repo = "CS400-Lib";
+      owner = "nmmull";
+      # Update the git commit hash here, then, randomly change one alphabet in
+      # the `hash` field below to force Nix to recalculate its hash. Run `nix-shell`.
+      # Nix will tell you your current hash is wrong and provide you with the
+      # hash it calculated. Now paste that hash into the `hash` field below.
+      rev = "d2c67135b6a95d38fc3d75dd891e817472b05dac";
+      hash = "sha256-0mZDB3qBV/cWJe1nKe8m3rdZJMt4oL2TM/FZKMWlcu8=";
+    };
+
+    meta = {
+      description = "Class library for BU CAS CS400 A1 (Spring 2024)";
+    };
+
+    buildPhase = ''
+    ln -s cs400-lib.agda src/CS400-Lib.agda 
+    agda src/CS400-Lib.agda
+    '';
+  };
+
   # Agda uses the AGDA_DIR environmental variable to determine where to load
   # default libraries from. This should have a few files in it, we only need 
   # the "defaults" files generated below.
@@ -21,6 +50,7 @@ let
     installPhase = ''
       mkdir $out
       echo "standard-library" >> $out/defaults
+      echo "cs400-lib" >> $out/defaults
     '';
   };
 in
@@ -28,6 +58,7 @@ with pkgs; mkShell {
   buildInputs = [
     (agda.withPackages (ps: [
       ps.standard-library
+      cs400-lib
     ]))
   ];
 
